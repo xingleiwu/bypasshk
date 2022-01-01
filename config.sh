f46207dd385a1470a8afa530cf90aba88b613a4e
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2022-01-01 15:57:06 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -35,7 +35,7 @@ cat << EOF > /usr/local/etc/xray/config.json
                 "network": "ws",
                 "allowInsecure": false,
                 "wsSettings": {
-                  "path": "/$ID-vless?ed=2048"
+                  "path": "/$ID-vless"
                 }
             }
         },
@@ -56,7 +56,7 @@ cat << EOF > /usr/local/etc/xray/config.json
                 "network": "ws",
                 "allowInsecure": false,
                 "wsSettings": {
-                  "path": "/$ID-trojan?ed=2048"
+                  "path": "/$ID-trojan"
                 }
             }
         }
